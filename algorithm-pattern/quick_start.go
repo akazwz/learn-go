4da8ea238cad6d03e1f786bded77ac6ce1983dd4
akez <1791189210@qq.com>
2021-08-05 13:45:13 +0800

    algorithm

--- algorithm-pattern/quick_start.go
+++ algorithm-pattern/quick_start.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	index := strStr("this is stack", "ck")
-	fmt.Println(index)
+	_ = strStr("this is stack", "ck")
+	nums := make([]int, 0)
+	nums = append(nums, 1, 2, 3, 4, 5, 6, 7)
+	_ = subsets(nums)
+	subsetsMy(nums)
 }
 
-// strStr 28
+// strStr 28  196 30.30%
 func strStr(haystack, needle string) int {
 	if len(needle) == 0 {
 		return 0
@@ -31,4 +34,38 @@ func strStr(haystack, needle string) int {
 	}
 
 	return -1
+}
+
+func subsetsMy(nums []int) [][]int {
+	result := make([][]int, 0)
+	list := make([]int, 0)
+	for _, num := range nums {
+		list = append(list, num)
+		for _, listItem := range list {
+			list = append(list, listItem)
+			result = append(result, list)
+		}
+	}
+	fmt.Println(len(result))
+	fmt.Println(len(list))
+	return [][]int{}
+}
+
+// 78 子集 0ms  100%
+func subsets(nums []int) [][]int {
+	result := make([][]int, 0)
+	list := make([]int, 0)
+	backtrack(nums, 0, list, &result)
+	return result
+}
+
+func backtrack(nums []int, pos int, list []int, result *[][]int) {
+	ans := make([]int, len(list))
+	copy(ans, list)
+	*result = append(*result, ans)
+	for i := pos; i < len(nums); i++ {
+		list = append(list, nums[i])
+		backtrack(nums, i+1, list, result)
+		list = list[0 : len(list)-1]
+	}
 }
