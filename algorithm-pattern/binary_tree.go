4c674538157ce7d668a6be307b89f4d39d418f2d
akez <1791189210@qq.com>
2021-08-16 09:52:33 +0800

    102 二叉树的层序遍历

--- algorithm-pattern/binary_tree.go
+++ algorithm-pattern/binary_tree.go
@@ -209,3 +209,32 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	}
 	return nil
 }
+
+// 102. 二叉树的层序遍历  0 ms, faster than 100.00%
+// Binary Tree Level Order Traversal
+func levelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		list := make([]int, 0)
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			level := queue[0]
+			queue = queue[1:]
+			list = append(list, level.Val)
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		result = append(result, list)
+	}
+	return result
+}
