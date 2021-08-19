8e9b2bc23c9fcb80366e1f9ac1b86ec1b2d1c8c9
akez <1791189210@qq.com>
2021-08-19 16:49:55 +0800

    98. 验证二叉搜索树

--- algorithm-pattern/binary_tree.go
+++ algorithm-pattern/binary_tree.go
@@ -293,3 +293,26 @@ func reverseOne(nums []int) {
 		nums[i], nums[len(nums)-1-i] = nums[len(nums)-1-i], nums[i]
 	}
 }
+
+// 98. 验证二叉搜索树  4 ms, faster than 96.22%
+// 98. Validate Binary Search Tree
+func isValidBST(root *TreeNode) bool {
+	result := make([]int, 0)
+	inOrder(root, &result)
+	// check order
+	for i := 0; i < len(result)-1; i++ {
+		if result[i] >= result[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+func inOrder(root *TreeNode, result *[]int) {
+	if root == nil {
+		return
+	}
+	inOrder(root.Left, result)
+	*result = append(*result, root.Val)
+	inOrder(root.Right, result)
+}
