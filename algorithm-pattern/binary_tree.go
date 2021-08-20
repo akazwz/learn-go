1b6b63a1448cf564ff607fc2691f698cff08b270
akez <1791189210@qq.com>
2021-08-20 13:58:52 +0800

    701. 二叉搜索树中的插入操作

--- algorithm-pattern/binary_tree.go
+++ algorithm-pattern/binary_tree.go
@@ -316,3 +316,18 @@ func inOrder(root *TreeNode, result *[]int) {
 	*result = append(*result, root.Val)
 	inOrder(root.Right, result)
 }
+
+// 701. 二叉搜索树中的插入操作 24 ms, faster than 97.75%
+// 701. Insert into a Binary Search Tree
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		root = &TreeNode{Val: val}
+		return root
+	}
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
